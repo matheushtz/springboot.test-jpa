547cc713955147d08b8b15112f3eda30cd9595cc
Matheus Hortiz <matheus.hortiz@gmail.com>
2026-01-19 13:46:14 -0300

    Projetinho criado

---
+++ src/main/java/com/mazin/cursinho/CursinhoApplication.java
@@ -0,0 +1,13 @@
+package com.mazin.cursinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mazin/cursinho/CursinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazin.cursinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
