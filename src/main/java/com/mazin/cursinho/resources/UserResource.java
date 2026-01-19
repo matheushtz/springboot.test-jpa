518d5ce871edf8c54a5e10049c6c1b72a74edbe5
Matheus Hortiz <matheus.hortiz@gmail.com>
2026-01-19 17:26:06 -0300

    UserEntity and Resources

---
+++ src/main/java/com/mazin/cursinho/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.mazin.cursinho.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.mazin.cursinho.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findall(){
+        User u = new User(1L, "Matheus", "matheus@example.com", 
+        "123456789", "password");
+        return ResponseEntity.ok().body(u);
+
+    }
+}
